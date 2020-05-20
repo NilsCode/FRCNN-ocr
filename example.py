7dae442d8fbab4f091dbacc7a58b377627f2a493
NilsCode <nileshsutar09@gmail.com>
2020-05-20 20:44:15 +0530

    Added examples for both test and train dataset

--- example.py
+++ example.py
@@ -9,17 +9,19 @@ from Completedataset import *
 
 # Create Instance of class to generate Dataset of Images for word detection - (Num Images = 10)
 TrainDataset = ImageWordsDataset(800)
-
+TestDataset =  ImageWordsDataset(100)
 # Generate image objects which are to be used to generate dataset containing 
 # letter boundingboxes
-imgobjects = TrainDataset.generate_img_data(letterdata = True,skip_percentage = 0.5,noise_add = 0.5)
+imgobjectstrain = TrainDataset.generate_img_data(letterdata = False,skip_percentage = 0.5,noise_add = 0.5,word_drawbndbox = True,letter_drawbndbox = False)
+imgobjectstest = TestDataset.generate_img_data(letterdata = False,skip_percentage = 0.5,noise_add = 0.5,word_drawbndbox = True,letter_drawbndbox = False)
 
 # Generate labels in csv format(as per tensorflow object detection api format)
 TrainDataset.generatelabels("traindata.csv","images//","images//train//")
+TestDataset.generatelabels("testdata.csv","images//","images//test//")
 
 # Write images to folder- (location specified when generating labels)
 TrainDataset.write_images_to_folder()
-
+TestDataset.write_images_to_folder()
 # LetterTrainDataset = letterBndBoxDataset(imgobjects)
 
 # LetterTrainDataset.generate_labels(None, "images\\train\\")
